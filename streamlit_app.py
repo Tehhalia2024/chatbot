20d389cde85467d7e58604935102958c9bfcfab2
Tehhalia2024 <kopioks2016@gmail.com>
2024-08-12 20:39:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,32 @@
 import streamlit as st
-import numpy as np
+
 # from openai import OpenAI
 
-with st.chat_message("assistant"):
-    st.write("Hello human 👋")
-    st.bar_chart(np.random.randn(30,))
+at.title("Echo Bot")
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f'User has sent the following prompt: {prompt}')
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]:
+        st.markdown(message["content"])
 
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt))
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
 # # Show title and description.
 # st.title("💬 KopiO Kosong Chatbot")
