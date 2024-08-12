4135108465f694f066e37bcf11ab0d3b322ce100
Tehhalia2024 <kopioks2016@gmail.com>
2024-08-12 20:28:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ with st.chat_message("assistant"):
 
 prompt = st.chat_input("Say something")
 if prompt:
-    st.write(f"User has sent the following prompt: {prompt}')
+    st.write(f'User has sent the following prompt: {prompt}')
 
 
 
